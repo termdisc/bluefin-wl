4e7c60ef3278a94066376737682ef757f43c1de6
termdisc <outphase@gmail.com>
2025-03-28 11:59:07 -0500

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,7 +12,7 @@ set -ouex pipefail
 # this installs a package from fedora repos
 # dnf5 install -y tmux 
 dnf5 -y install \
-    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm \
+#    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm \
     https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm
     
 dnf5 -y install \
